e144aade7637b24e161664a079ae86b21e663b40
Mofil <mofila@amazon.com>
2025-09-04 20:48:40 +0530

    Create Microsites_OOTO_meta.js

---
+++ Microsites_OOTO_meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Microsites_OOTO
+// @namespace    https://amazon.com/
+// @version      0.1
+// @description  Schedule Quick connects and send OOTO from the phonetool page - Microsites
+// @author       @mofila
+// @match        https://phonetool.amazon.com/users/*
+// @match        https://connect.amazon.com/users/*
+// @updateURL    https://raw.githubusercontent.com/Mofi-l/Microsites_OOTO/main/Microsites_OOTO_meta.js
+// @downloadURL  https://raw.githubusercontent.com/Mofi-l/Microsites_OOTO/main/Microsites_OOTO_user.js
+// @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
+// ==/UserScript==
