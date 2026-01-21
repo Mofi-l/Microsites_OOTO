7a819f2bd7d64e5218f1ff0b4e1caeaaf6bb99db
Mofil <mofila@amazon.com>
2026-01-21 15:38:32 +0530

    Update version and add new match patterns

--- Microsites_OOTO_meta.js
+++ Microsites_OOTO_meta.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Microsites_OOTO
 // @namespace    https://amazon.com/
-// @version      0.1
+// @version      0.2
 // @description  Schedule Quick connects and send OOTO from the phonetool page - Microsites
 // @author       @mofila
 // @match        https://phonetool.amazon.com/users/*
 // @match        https://connect.amazon.com/users/*
+// @match        https://outlook.office.com/*
 // @updateURL    https://raw.githubusercontent.com/Mofi-l/Microsites_OOTO/main/Microsites_OOTO_meta.js
 // @downloadURL  https://raw.githubusercontent.com/Mofi-l/Microsites_OOTO/main/Microsites_OOTO_user.js
 // @grant        GM.xmlHttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @grant        unsafeWindow
 // ==/UserScript==
